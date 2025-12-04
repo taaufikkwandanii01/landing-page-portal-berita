63055ca04d32c823fa454d428fd065bae80a575d
Taufik Wandani <taaufikkwandanii01@gmail.com>
2025-12-04 09:20:13 +0700

    feat: delete category in MainLayout Article

--- src/pages/Artikel/[id].tsx
+++ src/pages/Artikel/[id].tsx
@@ -19,7 +19,7 @@ export default function ArtikelPage() {
 
   return (
     <>
-      <MainLayout category={[category as string]}>
+      <MainLayout>
         <div className="max-w-3xl mx-auto p-4">
           <h1 className="text-2xl font-bold mb-4">{title}</h1>
           <p className="text-sm text-gray-600 mb-4">
